9020c55d261955d99ddaf61c71bcf41c32b40ff6
7rebux <58339645+7rebux@users.noreply.github.com>
2023-05-21 13:55:02 +0200

    Use tweepy

--- main.py
+++ main.py
@@ -5,98 +5,54 @@ __author__ = "Nils Osswald"
 from dotenv import load_dotenv
 import os
 from instagrapi import Client
-from requests_oauthlib import OAuth1Session
-import json
+import tweepy
 
 load_dotenv()
 
 instagram = Client()
+twitter = None
 
 def instagram_login():
     print("Logging in to Instagram..")
     instagram.login(
-        os.environ.get('INSTAGRAM_USER'),
-        os.environ.get('INSTAGRAM_PASS')
+        os.environ.get("INSTAGRAM_USER"),
+        os.environ.get("INSTAGRAM_PASS")
     )
     print("Successfully logged in!")
 
 def instagram_post(image, description, hashtags):
     print(f"Uploading {image} to Instagram..")
-    media = instagram.photo_upload(
+    post = instagram.photo_upload(
         image,
         f"{description}\n\n{hashtags}"
     )
-    print(f"Successfully uploaded to https://www.instagram.com/p/{media.code}")
+    print(f"Successfully uploaded to https://www.instagram.com/p/{post.code}")
 
+def twitter_login():
+    print("Logging in to Twitter..")
+    global twitter
+    twitter = tweepy.Client(
+        bearer_token=os.environ.get("TWITTER_BEARER_TOKEN"),
+        consumer_key=os.environ.get("TWITTER_CONSUMER_KEY"),
+        consumer_secret=os.environ.get("TWITTER_CONSUMER_SECRET"),
+        access_token=os.environ.get("TWITTER_ACCESS_TOKEN"),
+        access_token_secret=os.environ.get("TWITTER_ACCESS_TOKEN_SECRET"),
+        return_type=tweepy.Response,
+        wait_on_rate_limit=True
+    )
+    print(f"Successfully logged in!")
+
+def twitter_post(image, description):
+    print(f"Uploading {image} to Twitter..")
+    id = twitter.create_tweet(text=description,).data["id"]
+    print(f"Successfully uploaded to https://twitter.com/_NFTalert_/status/{id}")
+
+description = "Uploaded using nft-alert"
 with open("instagram_hashtags.txt", "r") as file:
     hashtags = " ".join(file.read().splitlines())
-description = "Uploaded using instagrapi for python to @nft_alert_"
 
 instagram_login()
 instagram_post("./image.jpg", description, hashtags)
 
-consumer_key = os.environ.get("TWITTER_KEY")
-consumer_secret = os.environ.get("TWITTER_SECRET")
-
-# Be sure to add replace the text of the with the text you wish to Tweet. You can also add parameters to post polls, quote Tweets, Tweet with reply settings, and Tweet to Super Followers in addition to other features.
-payload = {"text": "Posted from nft-alert"}
-
-# Get request token
-request_token_url = "https://api.twitter.com/oauth/request_token?oauth_callback=oob&x_auth_access_type=write"
-oauth = OAuth1Session(consumer_key, client_secret=consumer_secret)
-
-try:
-    fetch_response = oauth.fetch_request_token(request_token_url)
-except ValueError:
-    print(
-        "There may have been an issue with the consumer_key or consumer_secret you entered."
-    )
-
-resource_owner_key = fetch_response.get("oauth_token")
-resource_owner_secret = fetch_response.get("oauth_token_secret")
-print("Got OAuth token: %s" % resource_owner_key)
-
-# Get authorization
-base_authorization_url = "https://api.twitter.com/oauth/authorize"
-authorization_url = oauth.authorization_url(base_authorization_url)
-print("Please go here and authorize: %s" % authorization_url)
-verifier = input("Paste the PIN here: ")
-
-# Get the access token
-access_token_url = "https://api.twitter.com/oauth/access_token"
-oauth = OAuth1Session(
-    consumer_key,
-    client_secret=consumer_secret,
-    resource_owner_key=resource_owner_key,
-    resource_owner_secret=resource_owner_secret,
-    verifier=verifier,
-)
-oauth_tokens = oauth.fetch_access_token(access_token_url)
-
-access_token = oauth_tokens["oauth_token"]
-access_token_secret = oauth_tokens["oauth_token_secret"]
-
-# Make the request
-oauth = OAuth1Session(
-    consumer_key,
-    client_secret=consumer_secret,
-    resource_owner_key=access_token,
-    resource_owner_secret=access_token_secret,
-)
-
-# Making the request
-response = oauth.post(
-    "https://api.twitter.com/2/tweets",
-    json=payload,
-)
-
-if response.status_code != 201:
-    raise Exception(
-        "Request returned an error: {} {}".format(response.status_code, response.text)
-    )
-
-print("Response code: {}".format(response.status_code))
-
-# Saving the response as JSON
-json_response = response.json()
-print(json.dumps(json_response, indent=4, sort_keys=True))
+twitter_login()
+twitter_post("./image.jpg", description)
